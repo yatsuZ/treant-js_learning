bc088681358501250d7c6b19cf3a95b4ca3872cd
yatsuZ <yassine.zaoui28062003@gmail.com>
2025-06-28 07:36:11 +0200

    TO DO

--- code/index.js
+++ code/index.js
@@ -1,3 +1,6 @@
 import chalk from 'chalk';
 
-console.log(chalk.green.bold('HEllO world'));
+console.log(chalk.green.bold('HELLO WORLD!!\n\n'));
+console.log(chalk.yellow('1. cree une permier page daccueil avec ejs'));
+console.log(chalk.yellow('2. Faire les redirection vers les autres pages test'));
+console.log(chalk.yellow('3. Utilise la techno'));
