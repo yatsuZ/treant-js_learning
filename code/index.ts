1899767356a16314662ec948cb5f807aa9ad1522
yassine <yassine.zaoui28062003@gmail.com>
2025-06-30 13:47:27 +0200

    Update avec ts

--- code/index.ts
+++ code/index.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+
 console.log(chalk.green.bold('HELLO WORLD!!\n\n'));
 console.log(chalk.yellow('1. cree une permier page daccueil avec ejs'));
 console.log(chalk.yellow('2. Faire les redirection vers les autres pages test'));
