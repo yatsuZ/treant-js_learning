bfc694dc9c167a7872b2dedaa486be4218214fcb
yassine <yassine.zaoui28062003@gmail.com>
2025-07-01 16:16:10 +0200

    A present faire les arbres

--- code/routes/route.ts
+++ code/routes/route.ts
@@ -1,21 +1,49 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-import '@fastify/view';
+import { FastifyRequest, FastifyReply } from "fastify";
+import "@fastify/view";
+
+interface RouteConfig {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  url: string;
+  view: string;
+  statusCode?: number;
+}
+
+const viewRoutes: RouteConfig[] = [
+  { method: "GET", url: "/", view: "default.ejs" },
+  {
+    method: "GET",
+    url: "/ExempleSimple",
+    view: "examples/ExempleSimple/main.ejs",
+  },
+  {
+    method: "GET",
+    url: "/ArbreFamille",
+    view: "examples/arbre-familial/main.ejs",
+  },
+  {
+    method: "GET",
+    url: "/ArbreTournoi",
+    view: "examples/tournoi-jeux/main.ejs",
+  },
+];
 
 export const routes = [
-	{
-		method: 'GET',
-		url: '/',
-		handler: async (_: FastifyRequest, reply: FastifyReply) => {
-			return reply.view('default.ejs');
-		},
-	},
-	// 404 fallback route (should be last)
-	// {
-	// 	method: 'GET',
-	// 	url: '/*',
-	// 	handler: async (_: FastifyRequest, reply: FastifyReply) => {
-	// 		reply.code(404);
-	// 		return reply.view('layouts/index.ejs', { notFound: true });
-	// 	},
-	// },
+  ...viewRoutes.map((route) => ({
+    method: route.method,
+    url: route.url,
+    handler: async (_: FastifyRequest, reply: FastifyReply) => {
+      if (route.statusCode) reply.code(route.statusCode);
+      return reply.view(route.view);
+    },
+  })),
+
+  // Fallback 404 route
+  {
+    method: "GET",
+    url: "/*",
+    handler: async (_: FastifyRequest, reply: FastifyReply) => {
+      reply.code(404);
+      return reply.view("404.ejs");
+    },
+  },
 ];
